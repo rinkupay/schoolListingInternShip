52167816300ce6a227f0c6229aa0b6b6c5375083
Rinku Murmu <rinkumurmupay@gmail.com>
2025-09-05 22:17:55 +0530

    added redme file

--- app/addSchool/page.tsx
+++ app/addSchool/page.tsx
@@ -88,7 +88,7 @@ export default function AddSchoolPage() {
 
         <button
           type="submit"
-          className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 disabled:opacity-50"
+          className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 disabled:opacity-50 cursor-pointer"
           disabled={loading}
         >
           {loading ? "Submitting..." : "Submit"}
